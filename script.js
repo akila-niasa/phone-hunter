be17b3811737d276d688501d747a1e3d164d204b
akila-niasa <akilaniasa@gmail.com>
2022-03-01 00:07:39 +0600

    first added

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ const displayPhone=(allphones)=>{
     phones.forEach(phone=>{
         const div=document.createElement("div")
         div.classList.add('col')
-        div.innerHTML=`<div class="card">
+        div.innerHTML=`
+      <div class="card">
         <img class='img-fluid' src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
@@ -40,3 +41,35 @@ const loadPhoneDetail=(id)=>{
   .then(data=>displayPhoneDetail(data.data))
 }
 
+const displayPhoneDetail=(Details)=>{
+  console.log(Details);
+  const phoneDetail=document.getElementById("phone-details")
+  const div=document.createElement("div")
+  div.innerHTML=`
+  <div class="row g-0">
+  <div class="col-md-4">
+    <img src="${Details.image}" class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h2 class="card-title">${Details.name}</h2>
+      <h5>${Details.releaseDate?Details.releaseDate:'release date no found'}</h5>
+      <p class="card-text"><small class="text-muted">ChipSet: ${Details.mainFeatures.chipSet}</small></p>
+      <p class="card-text"><small class="text-muted">displaySize: ${Details.mainFeatures.displaySize}</small></p>
+      <p class="card-text"><small class="text-muted">memory: ${Details.mainFeatures.memory}</small></p>
+      <p class="card-text"><small class="text-muted">storage: ${Details.mainFeatures.storage}</small></p>
+      <p class="card-text"><small class="text-muted">sensors: ${Details.mainFeatures.sensors}</small></p>
+
+      ${Details.others?`<p class="card-text"><small class="text-muted">Bluetooth: ${Details.others.Bluetooth}</small></p>
+      <p class="card-text"><small class="text-muted">GPS: ${Details.others.GPS}</small></p>
+      <p class="card-text"><small class="text-muted">USB: ${Details.others.USB}</small></p>
+      <p class="card-text"><small class="text-muted">WLAN: ${Details.others.WLAN}</small></p>
+      <p class="card-text"><small class="text-muted">NFC: ${Details.others.NFC}</small></p>
+      <p class="card-text"><small class="text-muted">Radio: ${Details.others.Radio}</small></p>
+      `:''}
+      
+    </div>
+  </div>
+</div>`
+phoneDetail.appendChild(div)
+}
